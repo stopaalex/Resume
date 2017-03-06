02f7bc483d8d3ff13cd9316be3b12a837d75ea5c
stopaalex <stopa.alex@gmail.com>
2017-03-06 14:37:05 -0500

    Updated JS with JQ

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+$(document).ready(function(){
+    $(".logocontent").hide();
+    $(".logobtn").click(function(){
+        $(".logocontent").slideToggle(1000);
+    });
+  
+    $(".mmcontent").hide();
+    $(".mmbtn").click(function(){
+        $(".mmcontent").slideToggle(1000);
+    });
+  
+    $(".wdcontent").hide();
+    $(".wdbtn").click(function(){
+        $(".wdcontent").slideToggle(1000);
+    });
+  
+  $(".heroimg").slideUp(10000);
+});
